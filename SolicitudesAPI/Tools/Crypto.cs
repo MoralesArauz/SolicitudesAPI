e6a93c42d8cd1dd8f485c56a8469f53424f45a55
Adrian Morales <m-akuk@hotmail.com>
2022-04-12 23:46:17 -0600

    Agregando Usuarios desde el APP

---
+++ SolicitudesAPI/Tools/Crypto.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SolicitudesAPI.Tools
+{
+    public class Crypto
+    {
+        //Esta clase contiene los métodos necesarios para encripatar
+        //no solo contraseñas sino cualquier dato importante
+        //TODO: utilizar estas funciona para encriptar la cadena de conexión 
+
+        string LlavePersonalizadaApp = "Appkajhsdkjh672716762Dfg74";
+        string LlavePersonalizadaAPI = "APiklsjdnfjkfdf90rer83722";
+
+        public string DesEncriptarPassword(string Pass, bool UsarLlaveApp = true)
+        {
+            string R = string.Empty;
+
+            string LlaveAUsar = LlavePersonalizadaApp;
+
+            if (!UsarLlaveApp)
+            {
+                LlaveAUsar = LlavePersonalizadaAPI;
+            }
+
+#pragma warning disable SYSLIB0021 // Type or member is obsolete
+            using (TripleDESCryptoServiceProvider tripleDESCryptoService = new TripleDESCryptoServiceProvider())
+            {
+                using (MD5CryptoServiceProvider hashMD5Provider = new MD5CryptoServiceProvider())
+                {
+                    Byte[] byteHash = hashMD5Provider.ComputeHash(Encoding.UTF8.GetBytes(LlaveAUsar));
+
+                    tripleDESCryptoService.Key = byteHash;
+                    tripleDESCryptoService.Mode = CipherMode.ECB;
+
+                    Byte[] data = Convert.FromBase64String(Pass);
+
+                    R = Encoding.UTF8.GetString(tripleDESCryptoService.CreateDecryptor().TransformFinalBlock(data, 0, data.Length));
+
+                }
+            }
+#pragma warning restore SYSLIB0021 // Type or member is obsolete
+
+            return R;
+
+        }
+
+        public string EncriptarPassword(string Pass, bool UsarLlaveApp = true)
+        {
+            string R = string.Empty;
+
+            string LlaveAUsar = LlavePersonalizadaApp;
+
+            if (!UsarLlaveApp)
+            {
+                LlaveAUsar = LlavePersonalizadaAPI;
+            }
+
+#pragma warning disable SYSLIB0021 // Type or member is obsolete
+            using (TripleDESCryptoServiceProvider tripleDESCryptoService = new TripleDESCryptoServiceProvider())
+            {
+                using (MD5CryptoServiceProvider hashMD5Provider = new MD5CryptoServiceProvider())
+                {
+                    Byte[] byteHash = hashMD5Provider.ComputeHash(Encoding.UTF8.GetBytes(LlaveAUsar));
+
+                    tripleDESCryptoService.Key = byteHash;
+                    tripleDESCryptoService.Mode = CipherMode.ECB;
+
+                    Byte[] data = Encoding.UTF8.GetBytes(Pass);
+
+                    R = Convert.ToBase64String(tripleDESCryptoService.CreateEncryptor().TransformFinalBlock(data, 0, data.Length));
+
+                }
+            }
+#pragma warning restore SYSLIB0021 // Type or member is obsolete
+
+            return R;
+
+        }
+
+        public string EncriptarEnUnSentido(string Entrada)
+        {
+            string PorEncriptar = EncriptarPassword(Entrada);
+
+            PorEncriptar += "PalabraClave";
+
+#pragma warning disable SYSLIB0021 // Type or member is obsolete
+            SHA256CryptoServiceProvider ProveedorCrypto = new SHA256CryptoServiceProvider();
+#pragma warning restore SYSLIB0021 // Type or member is obsolete
+
+            //Descompone la cadenaDeEntrada en Bytes
+            byte[] BytesDeEntrada = Encoding.UTF8.GetBytes(PorEncriptar);
+
+            //Usando los bytes de la cadena de entrada crea el Hash
+            byte[] BytesConHash = ProveedorCrypto.ComputeHash(BytesDeEntrada);
+
+            StringBuilder Resultado = new StringBuilder();
+
+            //el for recorre cada byte del Hash y lo agrega a una cadena (stringbuilder)
+            for (int i = 0; i < BytesConHash.Length; i++)
+                Resultado.Append(BytesConHash[i].ToString("x2").ToLower());
+            // el x2 lo que hace es poner los caracteres hexadecimales con cierto formato.
+
+            return Resultado.ToString();
+
+        }
+    }
+}
